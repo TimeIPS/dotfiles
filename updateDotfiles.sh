c2f33be5faa309ad51a03c10619899befc1e1ab8
Jacob Walker <jwalker@timeips.com>
2013-04-30 16:00:56 -0500

    Fixing spf13 hook

--- updateDotfiles.sh
+++ updateDotfiles.sh
@@ -4,8 +4,8 @@ repo=${HOME}/.dotfiles
 cd $repo
 git pull origin master
 
-for file in $(find ${repo} -name ".*" -type f); do
+for file in $(find ${repo} -name ".*" -type f -exec basename {} \;); do
     ln -sf ${repo}/${file} ${HOME}/${file}
 done;
 
-sh <( curl git://github.com/TimeIPS/dotfiles.git -L )
+sh <( curl https://raw.github.com/TimeIPS/spf13-vim/3.0/bootstrap.sh -L )
