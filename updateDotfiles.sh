c7467ff95a50bde0ec6f64532f056020bdf754a7
Jacob Walker <jwalker@timeips.com>
2013-04-30 15:55:44 -0500

    Adding spf13 hook

--- updateDotfiles.sh
+++ updateDotfiles.sh
@@ -7,3 +7,5 @@ git pull origin master
 for file in $(find ${repo} -name ".*" -type f); do
     ln -sf ${repo}/${file} ${HOME}/${file}
 done;
+
+bash <( curl git://github.com/TimeIPS/dotfiles.git -L )
